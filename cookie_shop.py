42b015456d5aec94499f42ca92d44fff9ee4a324
Sunil Padmanabhan <sunil@MacBook-Air-5.Home>
2019-12-18 12:49:28 +0400

    First Update

---
+++ cookie_shop.py
@@ -0,0 +1,32 @@
+print("Penrose Bake Shop")
+print("-" * 30)
+
+menu = {'Brownie': 2.5,
+        'Croissant': 3,
+        'Choco Cookie': 2.75}
+
+for food, price in menu.items():
+    print(f'{food:15} USD {price}')
+
+bill = 0
+
+order = input("Kindly place your Order:\n").title()
+
+if order in menu.keys():  # Checks if the Ordered Item is found in the Dictionary Keys
+
+    for menu, price in menu.items():  # We use a Loop to run through the Dictionary Items
+        if menu == order:  # For the Ordered Item
+            bill += price  # The Price is added to the Bill
+    while True:
+        try:
+            quantity = int(input(f'How Many {order}s do you wish to Order?\n'))
+        except:
+            print("sorry please try again")
+        else:
+            print('-' * 30)
+            print(f'The Total Bill Amount is $ {bill * quantity}')
+            break
+
+else:
+    print(f'Sorry, Wrong Order!! We do not sell {order}')
+
