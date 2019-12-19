27c23991c2120d7b3679ec7a86052713872409df
Sunil Padmanabhan <sunil@MacBook-Air-5.Home>
2019-12-19 11:04:45 +0400

    Menu

---
+++ menu.py
@@ -0,0 +1,30 @@
+start_message='''
+welcome to the restaurant.
+
+order now
+____________________
+'''
+
+starters = {'tomato soup': 5,
+            'green chilly soup': 7,
+            'salad': 10}
+
+maincourse={'extra cheese pan pizza': 20,
+            'burger': 30,
+            'cheese burst pizza': 35}
+
+dessert={'cake': 30,'brownie': 30,
+         'choco chip': 27,
+         'choco lava cake':40}
+
+drinks={'pepsi':10,
+        'thumbs up':15,
+        'mango shake':17,
+        'coke':10}
+
+end_message = '''
+please give a good feedback only ,if you can't 100 fine.
+
+hope you enjoyed the dining
+___________________________
+'''
